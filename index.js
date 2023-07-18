8afa83d1f9723abaae8d9e98d30270c40907c827
Lana3315 <serookayalana@gmail.com>
2023-07-18 19:17:09 +0200

    update

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ $(document).ready(function () {
     $('.paralax_fog').css('opacity', o);
     var z_2 = 1 + (w / 5000000 * p);
     $('.paralax_montain_1 ').css('transform', 'scale(' + z_2 + ')');
+    var hr = w / 2000 * p_block;
+     var z_3 = 1 + (w * 0.000005 * p_block);
+    $('.paralax_montain_2 ').css('transform', 'translate3d(' + hr + 'px,0,0) scale(' + z_3 + ')');
+    
+    var hr_1 = w / 1500 * p_block;
+     var z_4 = 1 + (w * 0.00001 * p_block);
+    $('.paralax_montain_3 ').css('transform','translate3d('+hr_1+'px,0,0) scale(' + z_4 + ')');
   })
 })
